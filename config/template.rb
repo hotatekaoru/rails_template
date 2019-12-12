65d20fcd761557f3ad6d9fdac7732d1c8042eea2
mpg-kaoru-hotate <kaoru.hotate@medpeer.co.jp>
2019-12-12 13:49:43 +0900

    sidekiqの設定追加

--- config/template.rb
+++ config/template.rb
@@ -3,6 +3,7 @@ copy_file 'config/application.yml'
 copy_file 'config/initializers/0_settings.rb'
 copy_file 'config/initializers/kaminari_config.rb'
 copy_file 'config/initializers/rollbar.rb'
+copy_file 'config/initializers/sidekiq.rb'
 copy_file 'config/newrelic.yml'
 
 insert_into_file 'config/environments/development.rb', after: /\A/ do
@@ -10,5 +11,11 @@ insert_into_file 'config/environments/development.rb', after: /\A/ do
   # 明示的に読み込まないとdb:migrate時に落ちる
   require_relative '../initializers/0_settings.rb'
 
+  RUBY
+end
+
+insert_into_file 'config/application.rb', before: /^  end/ do
+  <<-'RUBY'
+    config.active_job.queue_adapter = :sidekiq
   RUBY
 end
